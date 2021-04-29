91348599752917048a1e1f744e45fcfd82062fb7
yangjia <yangjiadream@gmail.com>
2021-04-29 15:45:10 +0800

    main

---
+++ spring-cloud-provider1/src/main/java/com/example/springcloudprovider1/SpringCloudProvider1Application.java
@@ -0,0 +1,13 @@
+package com.example.springcloudprovider1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudProvider1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudProvider1Application.class, args);
+    }
+
+}
---
+++ spring-cloud-provider1/src/test/java/com/example/springcloudprovider1/SpringCloudProvider1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudprovider1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudProvider1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
