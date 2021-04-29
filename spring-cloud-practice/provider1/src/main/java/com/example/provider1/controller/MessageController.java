4a2d9acbafd6512d8e61bd33d8ecb9f97f1a228b
yangjia <yangjiadream@gmail.com>
2021-04-29 15:15:22 +0800

    getMessage

---
+++ spring-cloud-practice/provider1/src/main/java/com/example/provider1/controller/MessageController.java
@@ -0,0 +1,14 @@
+package com.example.provider1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @GetMapping("/getMessage")
+    public String getMessage(@RequestParam(value = "name", defaultValue = "yangjia", required = false) String name) {
+        return "hi:" + name + " I'm Provider1";
+    }
+}
