18058c2538994cb772168fd141ccee283a265c83
yangjia <yangjiadream@gmail.com>
2021-05-18 17:45:56 +0800

    初步提交gateway的路由功能

---
+++ spring-cloud-mygateway/src/main/java/com/example/springcloudmygateway/SpringCloudMygatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudmygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMygatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudMygatewayApplication.class, args);
+    }
+
+}
